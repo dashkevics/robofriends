539fec7a92fe29e7404efd9100255ab50a7b196a
dashkevics <dashkevics@gmail.com>
2020-10-13 14:50:21 +0600

    new version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import ErrorBoundry from './ErrorBoundry';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {  
+      robots: [],
+      searchfield: ''
+    }  
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users=> this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+  	this.setState({ searchfield: event.target.value})
+  }
+
+  render() {
+  	const filterRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={ filterRobots }/>
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
